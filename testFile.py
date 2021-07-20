35eb800b1e2c88b35f0c089b15bb5be3cd42f2a0
HyunseungLee-Travis <78584173+HyunseungLee-Travis@users.noreply.github.com>
2021-07-20 17:49:25 +0900

    removed typo

--- testFile.py
+++ testFile.py
@@ -1,19 +1,1 @@
-import multiprocessing
 
-
-def twoTime(num):
-    print(num * 2)
-
-
-def threTime(num):
-    print(num * 3)
-
-
-if __name__ == "__main__":
-    p1 = multiprocessing.Process(target=threTime, args=(10, ))
-    p2 = multiprocessing.Process(target=twoTime, args=(10, ))
-
-    p1.start()
-    p2.start()
-
-    print("Done!")
