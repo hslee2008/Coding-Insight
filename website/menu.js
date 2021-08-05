12cb5d016e4cd91811572603354ec2e874a8b744
HyunseungLee-Travis <78584173+HyunseungLee-Travis@users.noreply.github.com>
2021-08-05 14:13:50 +0900

    added stringmethod1 in website and updated md

--- website/menu.js
+++ website/menu.js
@@ -11,6 +11,7 @@ document.querySelector( "#menu" ).innerHTML = `
 <p><a href="datatype.html">(6) 데이터 형태</a></p>
 <p><a href="comment.html">(7) 주석 </a></p>
 <p><a href="string.html">(8) 문자 </a></p>
+<p><a href="stringmethod1.html">(8) 문자 메서드1 </a></p>
 
 `
 
