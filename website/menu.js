0999f249c31f9e6684a02d2343634d62afc19ca3
HyunseungLee-Travis <78584173+HyunseungLee-Travis@users.noreply.github.com>
2021-08-09 12:59:11 +0900

    updated menu.js

--- website/menu.js
+++ website/menu.js
@@ -11,6 +11,8 @@ document.querySelector( "#menu" ).innerHTML = `
 <a href="io.html">(4) 입출력</a>
 <a href="variable.html">(5) 변수</a>
 <a href="datatype.html">(6) 데이터 형태</a>
+<br />
+<br />
 <a href="comment.html">(7) 주석 </a>
 <a href="string.html">(8) 문자 </a>
 <a href="stringmethod1.html">(9) 문자 메서드1 </a>
@@ -27,6 +29,8 @@ document.querySelector( "#menu" ).innerHTML = `
 <a href="number.html">(14) 숫자 </a>
 <a href="operator.html">(15) 연산자 </a>
 <a href="numtype.html">(16) 숫자 종류 </a>
+<br />
+<br />
 <a href="nummethod.html">(17) 숫자 메서드 </a>
 <a href="list.html">(18) 리스트 </a>
 <a href="listmethod1.html">(19) 리스트 메서드 1 </a>
@@ -44,6 +48,8 @@ document.querySelector( "#menu" ).innerHTML = `
 <a href="set.html">(24) 집합 </a>
 <a href="setmethod.html">(25) 집합 메서드 </a>
 <a href="if.html">(26) 조건문 </a>
+<br />
+<br />
 <a href="ifmaster.html">(27) 조건문 마스터하기 </a>
 <a href="none.html">(28) None </a>
 <a href="function.html">(29) 함수 </a>
@@ -63,6 +69,8 @@ document.querySelector( "#menu" ).innerHTML = `
 <a href="forfunction.html">(36) for 반복문 함수</a>
 <a href="while.html">(37) while 반족문</a>
 <a href="oneliner.html">(38) 코드를 한 줄에</a>
+<br />
+<br />
 <a href="etcfunction.html">(39) 더 다양한 함수</a>
 <a href="module.html">(40) 모듈 기본</a>
 <a href="modulemaster.html">(41) 모듈 마스터하기</a>
