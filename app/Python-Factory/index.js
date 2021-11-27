ec946cd621172bba17c505ced6affa7bd485f1e3
Hyunseung <78584173+HyunseungLee-Travis@users.noreply.github.com>
2021-11-27 18:11:48 +0900

    Upgrade phone app to 2.0.0

--- app/Python-Factory/index.js
+++
@@ -1,5 +0,0 @@
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-registerRootComponent(App);
--- app/Python-Factory/metro.config.js
+++
@@ -1,9 +0,0 @@
-const {
-  getDefaultConfig
-} = require( 'expo/metro-config' );
-
-const config = getDefaultConfig( __dirname );
-
-config.transformer.minifierConfig.compress.drop_console = true;
-
-module.exports = config;
