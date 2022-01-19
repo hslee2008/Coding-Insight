2110f139250ac855721e454d1d2ea9551283ffc1
Hyunseung Lee <hyunseunglee2008@gmail.com>
2022-01-19 18:16:22 +0900

    Workbox added to service_worker.js

--- public_html/service_worker.js
+++ public_html/service_worker.js
@@ -1,20 +1,170 @@
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/6.4.2/workbox-sw.js"
-);
+const options = {
+  workboxURL:
+    "https://cdn.jsdelivr.net/npm/workbox-cdn@5.1.4/workbox/workbox-sw.js",
+  importScripts: [],
+  config: {
+    debug: false,
+  },
+  cacheOptions: {
+    cacheId: "python-kr-prod",
+    directoryIndex: "/",
+    revision: "naTeDaYyOgxH",
+  },
+  clientsClaim: true,
+  skipWaiting: true,
+  cleanupOutdatedCaches: true,
+  offlineAnalytics: false,
+  preCaching: [
+    {
+      revision: "naTeDaYyOgxH",
+      url: "/index.html",
+    },
+  ],
+  runtimeCaching: [
+    {
+      urlPattern: "global/",
+      handler: "CacheFirst",
+      method: "GET",
+      strategyPlugins: [],
+    },
+    {
+      urlPattern: "python/",
+      handler: "NetworkFirst",
+      method: "GET",
+      strategyPlugins: [],
+    },
+  ],
+  offlinePage: null,
+  pagesURLPattern: "python/",
+  offlineStrategy: "NetworkFirst",
+};
 
-workbox.routing.registerRoute(
-  /\.(?:css|js|html|json|png|jpg)$/,
-  new workbox.strategies.StaleWhileRevalidate({
-    "python-factory-service-worker-pwa": "assets",
-  })
-);
+importScripts(...[options.workboxURL, ...options.importScripts]);
 
-workbox.routing.registerRoute(
-  /\.(?:html|json|png|jpg|ico)$/,
-  new workbox.strategies.CacheFirst({
-    "python-factory-service-worker-pwa": "files",
-  })
-);
+initWorkbox(workbox, options);
+workboxExtensions(workbox, options);
+precacheAssets(workbox, options);
+cachingExtensions(workbox, options);
+runtimeCaching(workbox, options);
+offlinePage(workbox, options);
+routingExtensions(workbox, options);
+
+function getProp(obj, prop) {
+  return prop.split(".").reduce((p, c) => p[c], obj);
+}
+
+function initWorkbox(workbox, options) {
+  console.log("Workbox initialized");
+
+  if (options.config) {
+    workbox.setConfig(options.config);
+  }
+
+  if (options.cacheNames) {
+    workbox.core.setCacheNameDetails(options.cacheNames);
+  }
+
+  if (options.clientsClaim) {
+    s;
+    workbox.core.clientsClaim();
+  }
+
+  if (options.skipWaiting) {
+    workbox.core.skipWaiting();
+  }
+
+  if (options.cleanupOutdatedCaches) {
+    workbox.precaching.cleanupOutdatedCaches();
+  }
+
+  if (options.offlineAnalytics) {
+    // Enable offline Google Analytics tracking
+    workbox.googleAnalytics.initialize();
+  }
+}
+
+function precacheAssets(workbox, options) {
+  if (options.preCaching.length) {
+    workbox.precaching.precacheAndRoute(
+      options.preCaching,
+      options.cacheOptions
+    );
+  }
+}
+
+function runtimeCaching(workbox, options) {
+  console.log("RunTime Caching Started");
+  const requestInterceptor = {
+    requestWillFetch({ request }) {
+      if (request.cache === "only-if-cached" && request.mode === "no-cors") {
+        return new Request(request.url, {
+          ...request,
+          cache: "default",
+          mode: "no-cors",
+        });
+      }
+      return request;
+    },
+    fetchDidFail(ctx) {
+      ctx.error.message =
+        "[workbox] Network request for " +
+        ctx.request.url +
+        " threw an error: " +
+        ctx.error.message;
+      console.error(ctx.error, "Details:", ctx);
+    },
+    handlerDidError(ctx) {
+      ctx.error.message =
+        `[workbox] Network handler threw an error: ` + ctx.error.message;
+      console.error(ctx.error, "Details:", ctx);
+      return null;
+    },
+  };
+
+  for (const entry of options.runtimeCaching) {
+    const urlPattern = new RegExp(entry.urlPattern);
+    const method = entry.method || "GET";
+
+    const plugins = (entry.strategyPlugins || []).map(
+      (p) => new (getProp(workbox, p.use))(...p.config)
+    );
+
+    plugins.unshift(requestInterceptor);
+
+    const strategyOptions = {
+      ...entry.strategyOptions,
+      plugins,
+    };
+
+    const strategy = new workbox.strategies[entry.handler](strategyOptions);
+
+    workbox.routing.registerRoute(urlPattern, strategy, method);
+  }
+}
+
+function offlinePage(workbox, options) {
+  if (options.offlinePage) {
+    // Register router handler for offlinePage
+    workbox.routing.registerRoute(
+      new RegExp(options.pagesURLPattern),
+      ({ request, event }) => {
+        const strategy = new workbox.strategies[options.offlineStrategy]();
+        return strategy
+          .handle({
+            request,
+            event,
+          })
+          .catch(() => caches.match(options.offlinePage));
+      }
+    );
+  }
+}
+
+function workboxExtensions(workbox, options) {}
+
+function cachingExtensions(workbox, options) {}
+
+function routingExtensions(workbox, options) {}
 
 const sCacheName = "python-factory-service-worker-pwa";
 const aFilesToCache = [
